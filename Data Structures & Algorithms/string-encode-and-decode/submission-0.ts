8228ea03f16155ee16f9b7f29eed19ea7952ac92
Josh Allan <63212805+jallan07@users.noreply.github.com>
2026-06-11 15:32:12 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.ts
@@ -0,0 +1,60 @@
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs: string[]): string {
+    // If the input list is empty, return an empty string.
+    if (!strs.length || strs.length === 0) return ""
+
+    // Create an empty list to store the sizes of each string.
+    const wordLengths = []
+
+    // For each string, append its length to the sizes list.
+    for (let str of strs) {
+      wordLengths.push(str.length)
+    }
+
+    // Build a single string by:
+    // - Writing all sizes separated by commas.
+    // - Use a | as a delimiter between the sizes and input
+    // - Append all actual strings in order.
+    const stringifiedWordLengths = wordLengths.join(',');
+    const stringifiedInput = strs.join("");
+    const res = [...stringifiedWordLengths, "|", stringifiedInput].join('')
+    console.log({ res })
+
+    // Return the final encoded string.
+    return res;
+  }
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str: string): string[] {
+    // If the encoded string is empty, return an empty list.
+    if (!str.length || str.length === 0) return []
+
+    // mark the first instance of the delimiter
+    const delimiterPosition = str.indexOf('|');
+
+    // Extract everything before the first pipe
+    const sizesStr = str.substring(0, delimiterPosition);
+    const sizes = Array.from(sizesStr.split(','));
+
+    // Extract everything after the first pipe
+    let chars = str.substring(delimiterPosition + 1);
+
+    let result = []
+    for (let size of sizes) {
+      const numSize = parseInt(size);
+      // find the substrings (words) based on each stored size
+      result.push(chars.substring(0, numSize));
+      // reassign chars to be the string WITHOUT the previously decoded portion
+      chars = chars.slice(numSize)
+    }
+
+    return result
+  }
+}
